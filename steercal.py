6898730436fc81a5de4e3f7f76c83db4dc050b8c
G <gvnwltrs@protonmail.com>
2019-10-05 22:39:26 -0400

    Add files via upload

---
+++ steercal.py
@@ -0,0 +1,41 @@
+# steer sensor calibration tool 
+import math
+
+class steer:
+	def __init__(self):
+		pass
+
+'''
+# input values
+r = input("enter wheel radius:", r) 
+x = input("enter wheel turn distance:", x)
+
+adj = 0.0
+opp = x/2
+hyp = r 
+
+pi = math.pi
+rad = 180.0
+u_circ = 360.0
+circum = 2*pi*r
+
+def toDeg(val):
+	return (val*180)/pi
+
+def toRad():
+	pass
+
+def centralAngle():
+	# sine
+	return toDeg(opp / hyp) 
+
+# right wheel move right from zero |/
+def rightWheelTurn():
+
+# right wheel move right from zero |/
+def rightWheelTurn():
+
+
+
+
+'''
